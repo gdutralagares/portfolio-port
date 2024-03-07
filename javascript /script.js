abbd9d59ab2085bda88937c5cc3400002f98f8fa
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2024-03-07 18:18:20 -0300

    update

---
+++ javascript /script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+////////////
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
+//////////////
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName('tab-contents');
+function opentab(tabname){
+  for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
